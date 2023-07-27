cc30bf2bac1277e5e09b7e585a5b659a45dce642
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-07-27 14:50:25 +0300

    feat: add user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create.dto';
+import { UpdatePasswordDto } from './dto/update.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updatePassword: UpdatePasswordDto,
+  ) {
+    return this.userService.update(id, updatePassword);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.dto';
+import { UpdatePasswordDto } from './dto/update.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  findAll() {
+    return 'All User';
+  }
+
+  findOne(id: number) {
+    return 'Single User';
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return 'Create user';
+  }
+
+  update(id: number, updatePasswordDto: UpdatePasswordDto) {
+    return 'Update password';
+  }
+
+  delete(id: number) {
+    'Delete user';
+  }
+}
