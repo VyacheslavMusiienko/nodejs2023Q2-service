8b547e37cfc4c416f1814cc989a6c0aadd2faa23
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-07-28 16:38:25 +0300

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { TrackModule } from './track/track.module';
-import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
+import { DatabaseModule } from './database/database.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { TrackModule } from './track/track.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavoriteModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavoriteModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  readonly id: string;
+  readonly login: string;
+  readonly password: string;
+  readonly version: number;
+  readonly createdAt: number;
+  readonly updatedAt: number;
+}
