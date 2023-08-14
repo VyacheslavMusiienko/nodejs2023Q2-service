9dd643a8d8184775b21d6f957b2baebafe6b6aab
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-08-14 16:26:39 +0300

    fix: path to file

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthenticationService } from '../database/authentication/authentication.service';
 import { PrismaModule } from './../database/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, AuthenticationService],
+  providers: [UserService],
   imports: [PrismaModule],
 })
 export class UserModule {}
